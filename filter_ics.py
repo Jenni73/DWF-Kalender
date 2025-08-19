0536fceb3c501232986efac67a171ebcff8dfdbd
Jenni73 <j.zilch@jck.de>
2025-08-19 09:40:04 +0200

    Update filter_ics.py

--- filter_ics.py
+++ filter_ics.py
@@ -5,7 +5,7 @@ from ics import Calendar
 ICS_URL = "https://sked.lin.hs-osnabrueck.de/sked/jg/23SPS.ics"
 
 # Schlüsselwörter, nach denen gefiltert wird
-KEYWORDS = ["Supply Chain", "Controlling","Informationsmanagement", "Projektierung von ", "Usability", "Produktionsplanung","Projektmanagement", "Businessplanung" ]
+KEYWORDS = ["Supply Chain", "Controlling","Informationsmanagement", "Projektierung von ", "Usability", "Technologiemanagement","Projektmanagement", "Businessplanung" ]
 
 response = requests.get(ICS_URL)
 response.raise_for_status()
