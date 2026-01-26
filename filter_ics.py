c02f701dc5a5899c75c371949ae40dc9f00e0b77
Jenni73 <j.zilch@jck.de>
2026-01-26 13:54:38 +0100

    Update keywords for filtering in filter_ics.py

--- filter_ics.py
+++ filter_ics.py
@@ -6,8 +6,8 @@ from ics import Calendar
 ICS_URL = "https://sked.lin.hs-osnabrueck.de/sked/jg/23SPS.ics"
 
 # Schlüsselwörter, nach denen gefiltert wird                                                                                           
-KEYWORDS = ["Datenbank-Engineering", "IT-Sicherheit","Big Data", "Integrierte Managementsysteme A", "Marketing: Planung von Marketingstrategien B",  "Projektorientierte Unternehmens" ]
-# IT-Sicherheit A oder B(was passt besser, B= EBU); Integrierte Managementsysteme A (gibt auch B); Projekt UNFührung testen weil kein Projektmang
+KEYWORDS = ["Datenbank-Engineering", "IT-Sicherheit","Big Data", "Integrierte Managementsysteme A","Marketing: Internationales", "Marketing: Planung von Marketingstrategien B",  "Projektorientierte Unternehmens" ]
+# IT-Sicherheit A oder B(was passt besser, B= EBU); Integrierte Managementsysteme A (gibt auch B); Projekt UNFührung testen weil kein Projektmang-> vllt eher noch ein marketing
 
 def force_utf8(text):
     """Wandelt falsch decodierte Strings (fÃ¼) in echte Umlaute um."""
